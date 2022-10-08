d23701d05c95caaf3aa9be620e83aad7b251c359
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-08 15:37:04 -0600

    style added to the pokemon list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -26,19 +26,26 @@ const Home = () => {
 
     return (
       <>
-      <div className='flex h-32 items-center justify-center space-x-3'>
+      <div className='flex h-32 items-center justify-center space-x-3 bg-red-500'>
         <img 
           className='h-20'
           src={pokeball} 
           alt="pokeball image" 
         />
-        <div className='flex text-yellow-500 text-3xl'>PokeList</div>
+        <div className='flex text-yellow-500 text-3xl font-bold'>PokeList</div>
       </div>
-      <div className='grid grid-cols-9'>
-        {query.allPokemon.nodes.map((item) => (
+
+      <div className='grid grid-cols-9 mt-5'>
+        {query.allPokemon.nodes.slice(0, 9).map((item) => (
             <div>
-                <h1>{item.name}</h1>
-                <img src={item.image} alt={`${item.name} image`} />
+              <div className='grid bg-gray-100 h-36 w-36 rounded-lg place-items-center cursor-pointer hover:bg-gray-200'>
+                <img
+                  className='flex justify-center'               
+                  src={item.image} 
+                  alt={`${item.name} image`} 
+                />
+                <h1 className='text-gray-500'>{item.name}</h1>
+              </div>
             </div>
         ))}
       </div>
