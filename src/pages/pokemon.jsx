0a52ffc8a1b5a9dafe04fb264bcdf7ba95da23cb
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2022-10-12 02:38:33 -0600

    pokemon description added

--- src/pages/pokemon.jsx
+++ src/pages/pokemon.jsx
@@ -8,18 +8,31 @@ import axios from "axios";
 
 const Pokemon = ({ location }) => {
     const { state = {} } = location
-    const [data, setData] = useState();
-    
-    // useEffect(() => {
-    //     const getPokemonData = async () => {
-    //         await fetch(`https://pokeapi.co/api/v2/pokemon/${state.data.name}`)
-    //         .then(res => res.json())
-    //         .then(data => setData(data))
-    //       }
-    //       getPokemonData(); 
-    // },[])
+    const [data, setData] = useState('');
+    const [pokemonDescription, setPokemonDescription] = useState('');
+
+    const sprites_dreamWorld = data?.sprites?.other.dream_world.front_default;
+    const sprites_home = data?.sprites?.other.home.front_default;
+ 
+    useEffect(() => {
+        const getPokemonData = async () => {
+            await fetch(`https://pokeapi.co/api/v2/pokemon/${state.data.name}`)
+            .then(res => res.json())
+            .then(data => setData(data))
+          }
+          getPokemonData(); 
+    },[])
+
+    useEffect(() => {
+        const getPokemonDataDescription = async () => {
+            await fetch(`https://pokeapi.co/api/v2/pokemon-species/${state.data.name}`)
+            .then(res => res.json())
+            .then(response => setPokemonDescription(response))
+          }
+          getPokemonDataDescription(); 
+    },[])
   
-    // console.log("data", data)
+    console.log("pokemonDescription", pokemonDescription)
 
     return (
         <>
@@ -29,15 +42,25 @@ const Pokemon = ({ location }) => {
         <h3 className="flex text-gray-600 text-2xl justify-center pt-5 font-semibold">{state.data.name}</h3>
         <div className="flex justify-center p-5">
             <div className="grid w-60 h-full border rounded place-content-center p-5 bg-gray-100">
-                <img
-                    className="border rounded w-32" 
-                    src={state.data.image} 
-                    alt="pokemon image" 
-                />
+                {sprites_dreamWorld !== null ? (
+                    <img
+                        className="border rounded w-92 h-92" 
+                        src={sprites_dreamWorld} 
+                        alt="pokemon image" 
+                    />
+                ):(
+                    <img
+                        className="border rounded w-92" 
+                        src={sprites_home} 
+                        alt="pokemon image" 
+                    />
+                )
+                }
                 <PokemonStats stats={state.data.stats} />
             </div>
-            <div className="border w-96">
-                <h3 className="text-gray-600 text-sm m-2">Type</h3>
+            <div className="border w-96 p-3">
+                <p className="text-sm">{pokemonDescription?.flavor_text_entries?.[0].flavor_text}</p>
+                <h3 className="text-gray-600 text-sm mt-2 text-gray-500">Type</h3>
                 {state.data.types.map((item) => (
                     <p className="rounded-lg text-white w-20 text-center m-2" style={{backgroundColor: `${typeColor(item)}`}}>{item}</p>
                 ))}
