95fff219825ccc1f5a56c1bd28f9651c1a2ec7e2
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-02-28 18:17:59 -0600

    formatting code

--- src/types.ts
+++ src/types.ts
@@ -3,44 +3,44 @@ export interface HomeProps {
 }
 
 export interface AllPokemon {
-nodes: Node[];
-key: string;
-item: Node
+    nodes: Node[];
+    key: string;
+    item: Node
 }
 
 export interface Node {
-name:  string;
-stats: Stats;
-types: PokemonTypes[];
-image: string;
+    name:  string;
+    stats: Stats;
+    types: PokemonTypes[];
+    image: string;
 }
 
 export interface Stats {
-attack:          number;
-defense:         number;
-special_attack:  number;
-hp:              number;
-special_defense: number;
-speed:           number;
+    attack:          number;
+    defense:         number;
+    special_attack:  number;
+    hp:              number;
+    special_defense: number;
+    speed:           number;
 }
 
 export interface PokemonTypes {
-Bug: string;
-Dark: string;
-Dragon: string;
-Electric: string;
-Fairy: string;
-Fighting: string;
-Fire: string;
-Flying: string;
-Ghost: string;
-Grass: string;
-Ground: string;
-Ice: string;
-Normal: string;
-Poison: string;
-Psychic: string;
-Rock: string;
-Steel: string;
-Water: string;
+    Bug: string;
+    Dark: string;
+    Dragon: string;
+    Electric: string;
+    Fairy: string;
+    Fighting: string;
+    Fire: string;
+    Flying: string;
+    Ghost: string;
+    Grass: string;
+    Ground: string;
+    Ice: string;
+    Normal: string;
+    Poison: string;
+    Psychic: string;
+    Rock: string;
+    Steel: string;
+    Water: string;
 }
