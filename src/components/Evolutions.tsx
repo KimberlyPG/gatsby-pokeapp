e47e2e0bee82225d8524ec3952403a09b13fc57f
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-03-11 19:51:33 -0600

    adding styles to the evolutions component

--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -13,18 +13,18 @@ const Evolutions: FC<EvolutionProps> = ({ evolutionChain }) => {
 	return (
 		<div className="justify-center w-2/4 mt-4 mx-auto">
 			<p className="flex justify-center text-gray-800 font-semibold w-32 p-1 text-2xl">Evolutions</p> 
-			<div className="flex space-x-16 py-10 px-20 items-center bg-gray-200 bg-opacity-70 mb-5 rounded-tr-2xl rounded-bl-2xl">
+			<div className="flex py-10 px-20 items-center bg-gray-200 bg-opacity-70 mb-5 rounded-tr-2xl rounded-bl-2xl">
 				{evolutionChain.chain &&
 					<EvolutionCard url={evolutionChain.chain.species.url} name={evolutionChain.chain.species.name} />              
 				}
 				<div className="flex flex-col">
 					{evolutionChain?.chain?.evolves_to.length > 0 && evolutionChain?.chain?.evolves_to?.map((item) => (
-						<div key={item.species.name} className="flex flex-row space-x-16 items-center">
-							<AiOutlineRight className="text-4xl"/>
+						<div key={item.species.name} className="flex flex-row items-center">
+							<AiOutlineRight className="text-4xl mx-5"/>
 							<EvolutionCard url={item.species.url} name={item.species.name} />
 							{item.evolves_to.length > 0 && item.evolves_to?.map((element) => (
-								<div key={item.species.name}>
-									<AiOutlineRight className="text-4xl"/>
+								<div className='flex flex-row items-center' key={item.species.name}>
+									<AiOutlineRight className="text-4xl mx-5"/>
 									<EvolutionCard url={element.species.url} name={element.species.name} />
 								</div>
 							))} 
