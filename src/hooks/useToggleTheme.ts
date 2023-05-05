e30854e3c9063b6970a16a988f1335ebb4e0afa6
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-05-05 13:38:16 -0600

    cheking if window if defined for the local storage

--- src/hooks/useToggleTheme.ts
+++ src/hooks/useToggleTheme.ts
@@ -4,11 +4,15 @@ export function useToggleTheme () {
 	const [theme, setTheme] = useState<string>(localStorage.theme);
 	const colorTheme = theme === "dark" ? "light" : "dark";
 
+	const isBrowser = typeof window !== "undefined"
+
 	useEffect(() => {
 		const root = window.document.documentElement;
 		root.classList.remove(colorTheme);
 		root.classList.add(theme);
-		localStorage.setItem('theme', theme);
+		if (isBrowser) {
+			window.localStorage.setItem('theme', theme);
+		}
 	}, [theme, colorTheme]);
 
 	return [colorTheme, setTheme]
