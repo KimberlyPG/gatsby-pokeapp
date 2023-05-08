000df393d8c656c1c26df673df4fd17f47d53fff
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-05-08 13:52:45 -0600

    change in the plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
 			  mergeStyleHashes: true, 
 			  mergeDefaultDirectives: true,
 			  directives: {
-				"script-src": "'self' https",
+				"script-src": "'self' 'unsafe-inline'",
 				"style-src": "'self' 'unsafe-inline'",
 				"img-src": "'self' data: https:"
 			  }
