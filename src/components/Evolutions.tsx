2b80eeddbea93c2f684727c75322ab1a45c10b59
KimberlyPG <kim-kpgs98@hotmail.com>
2023-05-28 17:05:30 -0600

    pokemon evolutions distribution fixed

--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -11,14 +11,14 @@ interface EvolutionProps {
 
 const Evolutions: FC<EvolutionProps> = ({ evolutionChain }) => {
 	return (
-		<div className="justify-center md:w-2/4 mt-4 md:mx-auto">
+		<div className="justify-center md:w-[90%] lg:w-2/4 mt-4 md:mx-auto">
 			<p className="flex justify-center text-gray-800 dark:text-gray-100 font-semibold md:w-32 p-1 text-2xl">Evolutions</p> 
 			<div className="xxs:grid xxs:grid-cols-1 lg:flex place-items-center py-10 xxs:px-5 xl:px-20 items-center justify-center bg-gray-200 dark:bg-[#292929] 
 				bg-opacity-70 mb-5 rounded-tr-2xl rounded-bl-2xl">
 				{evolutionChain.chain &&
 					<EvolutionCard url={evolutionChain.chain.species.url} name={evolutionChain.chain.species.name} />              
 				}
-				<div className="xxs:grid xxs:auto-cols-max lg:flex lg:flex-col place-items-center">
+				<div className="xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col lg:place-items-center xxs:space-x-5 lg:space-x-0 w-fit">
 					{evolutionChain?.chain?.evolves_to.length > 0 && evolutionChain?.chain?.evolves_to?.map((item) => (
 						<div key={item.species.name} className="xxs:grid xxs:place-items-center lg:grid-flow-col">
 							<AiOutlineRight className="text-4xl mx-5 dark:text-gray-300 lg:flex xxs:hidden"/>
