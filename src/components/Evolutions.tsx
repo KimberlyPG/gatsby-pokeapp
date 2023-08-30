289bc1b16a2884d3c471621b72bd34130de25498
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-08-29 21:42:01 -0600

    chore: removing unused condition

--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -15,9 +15,7 @@ const Evolutions: FC<EvolutionProps> = ({ evolutionChain }) => {
 			<p className="flex justify-center text-gray-800 dark:text-gray-100 font-semibold md:w-32 p-1 text-2xl">Evolutions</p> 
 			<div className="xxs:grid xxs:grid-cols-1 lg:flex place-items-center py-10 xxs:px-5 xl:px-20 items-center justify-center bg-gray-200 dark:bg-[#272727] 
 				bg-opacity-70 mb-5 rounded-tr-2xl rounded-bl-2xl">
-				{evolutionChain.chain &&
-					<EvolutionCard url={evolutionChain.chain.species.url} name={evolutionChain.chain.species.name} />              
-				}
+				<EvolutionCard url={evolutionChain.chain.species.url} name={evolutionChain.chain.species.name} />              
 				<div className="xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col lg:place-items-center xxs:space-x-5 lg:space-x-0 w-fit">
 					{evolutionChain?.chain?.evolves_to.length > 0 && evolutionChain?.chain?.evolves_to?.map((item) => (
 						<div className="xxs:grid xxs:place-items-center lg:grid-flow-col">
