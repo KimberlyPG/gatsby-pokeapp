2aae66ce52adc39a9727885a79c1493e4e6fc33a
KimberlyPG <65067847+KimberlyPG@users.noreply.github.com>
2023-09-29 21:34:51 -0600

    chore: organizing the code

--- src/components/Evolutions.tsx
+++ src/components/Evolutions.tsx
@@ -13,14 +13,17 @@ const Evolutions: FC<EvolutionProps> = ({ evolutionChain }) => {
 	return (
 		<div className="justify-center md:w-[90%] lg:w-2/4 mt-4 md:mx-auto">
 			<p className="flex justify-center text-gray-800 dark:text-gray-100 font-semibold md:w-32 p-1 text-2xl">Evolutions</p> 
-			<div className="xxs:grid xxs:grid-cols-1 lg:flex place-items-center py-10 xxs:px-5 xl:px-20 items-center justify-center bg-gray-200 dark:bg-[#272727] 
+			<div className="xxs:grid xxs:grid-cols-1 lg:flex place-items-center py-10 xxs:px-5 xl:px-20 items-center justify-center 
+			bg-gray-200 dark:bg-[#272727] 
 				bg-opacity-70 mb-5 rounded-tr-2xl rounded-bl-2xl">
 				<EvolutionCard url={evolutionChain.chain.species.url} name={evolutionChain.chain.species.name} />              
-				<div className="xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col lg:place-items-center xxs:space-x-5 lg:space-x-0 w-fit">
+				<div className="xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col lg:place-items-center 
+				xxs:space-x-5 lg:space-x-0 w-fit">
 					{evolutionChain?.chain?.evolves_to.length > 0 && evolutionChain?.chain?.evolves_to?.map((item) => (
 						<div className="xxs:grid xxs:place-items-center lg:grid-flow-col">
 							<EvolutionContainer key={item.species.name} element={item} />
-							<div className='xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col xxs:space-x-5 lg:space-x-0 w-fit'>
+							<div className='xxs:grid xxs:grid-flow-col xxs:grid-rows-1 xxs:auto-cols-auto lg:flex lg:flex-col xxs:space-x-5 
+							lg:space-x-0 w-fit'>
 								{item.evolves_to.length > 0 && item.evolves_to?.map((element) => (
 									<EvolutionContainer key={item.species.name} element={element} />
 								))} 
